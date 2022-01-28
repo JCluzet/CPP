8f6c987a35d3ad9cb2c89e7871dd1746401a08f1
JCluzet <jo@cluzet.fr>
2022-01-28 20:59:44 +0100

    test

--- CPP04/ex02/main.cpp
+++ CPP04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 00:26:34 by jcluzet           #+#    #+#             */
-/*   Updated: 2022/01/12 19:41:44 by jcluzet          ###   ########.fr       */
+/*   Updated: 2022/01/28 20:58:57 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ int main(void) {
     const Animal* j = new Dog();
     const Animal* i = new Cat();
     //const Animal* test = new Animal(); //>> cannot be alaunch because abstract class
-    delete j
+    delete j;
     delete i;
 }
