0ffb714a9a2ff3f76df3e76711f26612da1487f4
JCluzet <jo@cluzet.fr>
2022-01-29 18:40:24 +0100

    chang min

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 00:26:34 by jcluzet           #+#    #+#             */
-/*   Updated: 2022/01/29 18:34:46 by jcluzet          ###   ########.fr       */
+/*   Updated: 2022/01/29 18:39:58 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,50 @@
 #include "Dog.hpp"
 #include "WrongCat.hpp"
 
-int main(void) {
-    std::cout << std::endl;
-    Animal *bernard = new Animal[100];
-    std::cout << std::endl;
-	for(int i = 0; i < 50; i++)
-    {
-        std::cout << "Dog : " << std::endl << std::endl;
-		bernard[i] = Dog();
-        std::cout << std::endl;
-    }
-    for(int i = 50; i < 100; i++)
-    {
-	    bernard[i] = Cat();
-    }
-    std::cout << std::endl << std::endl;
-    Animal *woof = new Animal[1];
+// int main(void) {
+//     std::cout << std::endl;
+//     Animal *bernard = new Animal[100];
+//     std::cout << std::endl;
+// 	for(int i = 0; i < 50; i++)
+//     {
+//         std::cout << "Dog : " << std::endl << std::endl;
+// 		bernard[i] = Dog();
+//         std::cout << std::endl;
+//     }
+//     for(int i = 50; i < 100; i++)
+//     {
+// 	    bernard[i] = Cat();
+//     }
+//     std::cout << std::endl << std::endl;
+//     Animal *woof = new Animal[1];
 
-    std::cout << std::endl << std::endl;
-    woof[0] = Dog();
+//     std::cout << std::endl << std::endl;
+//     woof[0] = Dog();
 
-    std::cout << std::endl << std::endl;
-    delete [] woof;
-    std::cout << std::endl << std::endl;
-	delete [] bernard;
-	return 0;
+//     std::cout << std::endl << std::endl;
+//     delete [] woof;
+//     std::cout << std::endl << std::endl;
+// 	delete [] bernard;
+// 	return 0;
+// }
+
+int main(void)
+{
+    Animal *(j[100]);
+    for (int i = 0; i < 50; i++)
+    {
+        j[i] = new Dog();
+    }
+    for (int i = 50; i < 100; i++)
+    {
+        j[i] = new Cat();
+    }
+    for (int i = 0; i < 100; i++)
+    {
+        j[i]->makeSound();
+    }
+    for (int i = 0; i < 100; i++)
+    {
+        delete j[i];
+    }
 }
