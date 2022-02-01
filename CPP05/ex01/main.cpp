4aaf93e5059d2b2c74a7587f966d4797f2b71e94
Joseph CLUZET <jcluzet@e2r7p6.clusters.42paris.fr>
2022-02-01 12:02:06 +0100

    still good

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -51,6 +51,70 @@ int main(void)
     catch (std::exception &e) {
         std::cerr << RED << "Output of Exception:  " << RESET << e.what() << std::endl;
     }
+    std::cout << "-----------------------------"<< std::endl;
+    std::cout << std::endl << MAGENTA << "Creation bureaucrat john lvl3" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    Bureaucrat john("john",3);
+    std::cout << std::endl << MAGENTA << "Creation of 3-sign | 12-exec form" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    
+    Form form1("form1", 3, 12);
+    try {
+        std::cout << std::endl << MAGENTA << "Use of beSigned with john : " << RESET;
+        form1.beSigned(john);
+    }
+    catch (std::exception &e) {
+        std::cerr << RED << "Output of Exception:  " << RESET << e.what() << std::endl;
+    }
+    std::cout << "-----------------------------"<< std::endl;
+
+
+    std::cout << "-----------------------------"<< std::endl;
+    std::cout << std::endl << MAGENTA << "Creation bureaucrat dann lvl3" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    Bureaucrat dann("dann",2);
+    std::cout << std::endl << MAGENTA << "Creation of 3-sign | 12-exec form" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    
+    Form form2("form2", 3, 12);
+    try {
+        std::cout << std::endl << MAGENTA << "Use of beSigned with dann " << RESET;
+        form2.beSigned(dann);
+    }
+    catch (std::exception &e) {
+        std::cerr << RED << "Output of Exception:  " << RESET << e.what() << std::endl;
+    }
+    std::cout << std::endl << std::endl << "-----------------------------"<< std::endl;
+
+    std::cout << "-----------------------------"<< std::endl;
+    std::cout << std::endl << MAGENTA << "Creation bureaucrat dann lvl3" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    Bureaucrat dany("dany",2);
+    std::cout << std::endl << MAGENTA << "Creation of 3-sign | 12-exec form" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    
+    Form form3("form3", 3, 12);
+    try {
+        std::cout << std::endl << MAGENTA << "Use of signForm with dany : " << RESET;
+        std::cout << std::endl;
+        dany.signForm(form3);
+    }
+    catch (std::exception &e) {
+        std::cerr << RED << "Output of Exception:  " << RESET << e.what() << std::endl;
+    }
+    std::cout << std::endl << std::endl << "-----------------------------"<< std::endl;
+
+    std::cout << "-----------------------------"<< std::endl;
+    std::cout << std::endl << MAGENTA << "Creation bureaucrat dann lvl3" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    Bureaucrat daniel("dany",2);
+    std::cout << std::endl << MAGENTA << "Creation of 1-sign | 12-exec form" << YELLOW << " using a try and catch : "<< RESET << std::endl;
+    
+    Form form4("form4", 1, 12);
+    try {
+        std::cout << std::endl << MAGENTA << "Use of signForm with dany : " << RESET;
+        std::cout << std::endl;
+        daniel.signForm(form4);
+    }
+    catch (std::exception &e) {
+        std::cerr << RED << "Output of Exception:  " << RESET << e.what() << std::endl;
+    }
+    std::cout << std::endl << std::endl << "-----------------------------"<< std::endl;
+
+
     std::cout << std::endl;
     return(0);
 }
