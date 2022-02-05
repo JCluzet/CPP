0cf4111b64c8f15ac1ca3883c73e187f1c18f456
JCluzet <jo@cluzet.fr>
2022-02-05 18:38:44 +0100

    push

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -6,12 +6,18 @@
 /*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 15:30:44 by jcluzet           #+#    #+#             */
-/*   Updated: 2022/02/05 15:45:14 by jcluzet          ###   ########.fr       */
+/*   Updated: 2022/02/05 18:37:21 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+
+template <typename T>
+void display(T const & elem)
+{
+    std::cout << elem << std::endl;
+}
 
 template <typename T>
 void iter(T *tab, int len, void fct(T const & elem))
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,18 +6,12 @@
 /*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 20:07:55 by jcluzet           #+#    #+#             */
-/*   Updated: 2022/02/05 15:51:12 by jcluzet          ###   ########.fr       */
+/*   Updated: 2022/02/05 18:37:58 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
-
-template <typename T>
-void display(T const & elem)
-{
-    std::cout << elem << std::endl;
-}
 
 int main(void)
 {
@@ -25,11 +19,11 @@ int main(void)
     
     int len = 10;
     for(int i = 0; i < len; i++)
-        tab[i] = 'A';
+        tab[i] = 'A' + i;
     iter(tab, len, display);
     std::cout << std::endl;
     for (int u = 0; u < len; u++)
-        tab[u] = 'B';
+        tab[u] = 'Z' - u;
     iter(tab, len, display);
     return(0);
 }
